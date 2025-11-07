10fa1ce754cfdaa6930c32e0149de529a129c727
priyansh3105 <priyanshpankaj3105@gmail.com>
2025-11-07 14:34:45 +0530

    Initial Commit

---
+++ TinyVitals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinyVitals
+//
+//  Created by user45 on 07/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TinyVitalsTests/TinyVitalsTests.swift
@@ -0,0 +1,17 @@
+//
+//  TinyVitalsTests.swift
+//  TinyVitalsTests
+//
+//  Created by user45 on 07/11/25.
+//
+
+import Testing
+@testable import TinyVitals
+
+struct TinyVitalsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
