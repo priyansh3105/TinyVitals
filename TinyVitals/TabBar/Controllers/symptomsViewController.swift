ce13296d462eca4f8f48eafc62aac554f759d87d
Vineeth Aitha <vineethaitha1234@gmail.com>
2025-11-08 23:55:55 +0530

    dummypush

--- TinyVitals/TabBar/Controllers/symptomsViewController.swift
+++ TinyVitals/TabBar/Controllers/symptomsViewController.swift
@@ -9,13 +9,35 @@ import UIKit
 
 class symptomsViewController: UIViewController {
 
+    @IBOutlet var symptomsSuperView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupGradient()
         // Do any additional setup after loading the view.
     }
     
-
+    func setupGradient() {
+        let gradientLayer = CAGradientLayer()
+        
+        // Top Color: Light Blue (#82C3FF) - Based on your color codes
+        // RGB: 130/255, 195/255, 255/255
+        let colorTop = UIColor(red: 0.51, green: 0.76, blue: 1.00, alpha: 1.0).cgColor
+        
+        // Bottom Color: White (#FFFFFF) - Based on your color codes
+        let colorBottom = UIColor.white.cgColor
+        
+        gradientLayer.colors = [colorTop, colorBottom]
+        
+        // Set for a Vertical Gradient (Top to Bottom)
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0) // Center Top
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)   // Center Bottom
+        
+        // Apply to the view
+        gradientLayer.frame = symptomsSuperView.bounds
+        symptomsSuperView.layer.insertSublayer(gradientLayer, at: 0)
+    }
     /*
     // MARK: - Navigation
 
