9d5efde2b5008fd2c6d754551a5f79b7e73e1f6a
Vineeth Aitha <vineethaitha1234@gmail.com>
2025-11-09 10:22:01 +0530

    Worked on homescreen vaccination card

---
+++ TinyVitals/TabBar/Controllers/CircularProgressView.swift
@@ -0,0 +1,20 @@
+//
+//  circularProgress.swift
+//  TinyVitals
+//
+//  Created by admin0 on 09/11/25.
+//
+
+import UIKit
+
+class CircularProgressView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
