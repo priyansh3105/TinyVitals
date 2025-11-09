7e4989adfde2e00cacebe2c069bd7e4164427548
priyansh3105 <priyanshpankaj3105@gmail.com>
2025-11-09 21:54:13 +0530

    parent profile screen

--- ParentProfile Screen/Controller/ParentProfileViewController.swift
+++ ParentProfile Screen/Controller/ParentProfileViewController.swift
@@ -9,11 +9,75 @@ import UIKit
 
 class ParentProfileViewController: UIViewController {
 
+    @IBOutlet weak var genderButton: UIButton!
+    
+    @IBOutlet weak var parentRelationButton: UIButton!
+    
+    @IBOutlet weak var childSelectionButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let male = UIAction(title: "Male") { _ in
+                    self.genderButton.setTitle("Male", for: .normal)
+                }
+                let female = UIAction(title: "Female") { _ in
+                    self.genderButton.setTitle("Female", for: .normal)
+                }
+                let others = UIAction(title: "Others") { _ in
+                    self.genderButton.setTitle("Others", for: .normal)
+                }
+
+                let genderMenu = UIMenu(title: "Select Gender", options: .displayInline, children: [male, female, others])
+                genderButton.menu = genderMenu
+                genderButton.showsMenuAsPrimaryAction = true
+                genderButton.changesSelectionAsPrimaryAction = true
+                genderButton.preferredMenuElementOrder = .fixed
+
+
+                // ========================
+                // MARK: - Parent Relation Menu
+                // ========================
+                let mother = UIAction(title: "Mother") { _ in
+                    self.parentRelationButton.setTitle("Mother", for: .normal)
+                }
+                let father = UIAction(title: "Father") { _ in
+                    self.parentRelationButton.setTitle("Father", for: .normal)
+                }
+                let othersRelation = UIAction(title: "Others") { _ in
+                    self.parentRelationButton.setTitle("Others", for: .normal)
+                }
+
+                let relationMenu = UIMenu(title: "Select Relation", options: .displayInline, children: [mother, father, othersRelation])
+                parentRelationButton.menu = relationMenu
+                parentRelationButton.showsMenuAsPrimaryAction = true
+                parentRelationButton.changesSelectionAsPrimaryAction = true
+                parentRelationButton.preferredMenuElementOrder = .fixed
+
+
+                // ========================
+                // MARK: - Child Selection Menu
+                // ========================
+                let child1 = UIAction(title: "Child-1") { _ in
+                    self.childSelectionButton.setTitle("Child-1", for: .normal)
+                }
+                let child2 = UIAction(title: "Child-2") { _ in
+                    self.childSelectionButton.setTitle("Child-2", for: .normal)
+                }
+                let child3 = UIAction(title: "Child-3") { _ in
+                    self.childSelectionButton.setTitle("Child-3", for: .normal)
+                }
+
+                let childMenu = UIMenu(title: "Select Child", options: .displayInline, children: [child1, child2, child3])
+                childSelectionButton.menu = childMenu
+                childSelectionButton.showsMenuAsPrimaryAction = true
+                childSelectionButton.changesSelectionAsPrimaryAction = true
+                childSelectionButton.preferredMenuElementOrder = .fixed
+
     }
+
 
 
     /*
