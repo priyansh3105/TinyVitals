40f5f20ffc7a0fc541552e25b4bb0f882681a6fe
Vineeth Aitha <vineethaitha1234@gmail.com>
2025-11-13 11:18:00 +0530

    merging updated tabBar with records feature

---
+++ TinyVitals/TabBar/Model/Record.swift
@@ -0,0 +1,17 @@
+//
+//  Record.swift
+//  TinyVitals
+//
+//  Created by admin0 on 11/11/25.
+//
+
+import Foundation
+
+struct Record {
+    let fileName: String
+    let source: String
+    let addedDate: Date
+    let type: String
+    let fileURL: URL?
+    let sectionName: String
+}
